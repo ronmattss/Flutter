5a62a903b206647ccdef3bcb5d8face0a5948058
ronmattss <31378489+ronmattss@users.noreply.github.com>
2019-07-04 19:49:32 +0800

    Update database_helper.dart

--- myapp/lib/helper_classes/database_helper.dart
+++ myapp/lib/helper_classes/database_helper.dart
@@ -6,35 +6,38 @@ import "package:sqflite/sqflite.dart";
 import "package:path_provider/path_provider.dart";
 
 class DatabaseHelper {
-  static final _databaseName ="TaskDatabase.db";
+  static final _databaseName = "TaskDatabase.db";
   static final _databaseVersion = 1;
 
   static final table = "task_table";
 
   static final columnId = 'id';
   static final columnTask = "task";
-  static final columnDescription ="description";
+  static final columnDescription = "description";
   static final columnCategory = "category";
-
 
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   static Database _database;
-  Future<Database> get database async{
-    if(_database != null) return _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
 
     _database = await _initDatabase();
     return _database;
   }
 
-  _initDatabase() async{
+  _initDatabase() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path,_databaseName);
-    return await openDatabase(path,version: _databaseVersion,onCreate:_onCreate,);
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
   }
 
-    Future _onCreate(Database db, int version) async {
+  Future _onCreate(Database db, int version) async {
     await db.execute('''
           CREATE TABLE $table (
             $columnId INTEGER PRIMARY KEY,
@@ -43,8 +46,7 @@ class DatabaseHelper {
             $columnCategory TEXT NOT NULL
           )
           ''');
-}
-
+  }
 
   // Helper methods
 
@@ -56,22 +58,22 @@ class DatabaseHelper {
     return await db.insert(table, row);
   }
 
-  // All of the rows are returned as a list of maps, where each map is 
+  // All of the rows are returned as a list of maps, where each map is
   // a key-value list of columns.
   Future<List<Map<String, dynamic>>> queryAllRows() async {
     Database db = await instance.database;
     return await db.query(table);
   }
 
-
   // All of the methods (insert, query, update, delete) can also be done using
   // raw SQL commands. This method uses a raw query to give the row count.
   Future<int> queryRowCount() async {
     Database db = await instance.database;
-    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $table'));
+    return Sqflite.firstIntValue(
+        await db.rawQuery('SELECT COUNT(*) FROM $table'));
   }
 
-  // We are assuming here that the id column in the map is set. The other 
+  // We are assuming here that the id column in the map is set. The other
   // column values will be used to update the row.
   Future<int> update(Map<String, dynamic> row) async {
     Database db = await instance.database;
@@ -79,21 +81,21 @@ class DatabaseHelper {
     return await db.update(table, row, where: '$columnId = ?', whereArgs: [id]);
   }
 
-  // Deletes the row specified by the id. The number of affected rows is 
+  // Deletes the row specified by the id. The number of affected rows is
   // returned. This should be 1 as long as the row exists.
   Future<int> delete(int id) async {
     Database db = await instance.database;
     return await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+  }
+  //Deletes the selected row
+  //Query via id
+  Future deleteRow(int id) async {
+    Database db = await instance.database;
+    return await db.delete(table, where: '$columnId =?', whereArgs: [id]);
+  }
 
-    
-}
-
-Future deleteRow(int id) async{
-Database db = await instance.database;
-return await db.delete(table, where: '$columnId =?',whereArgs: [id]);
-}
-
-
-
-
+  Future selectCategory(String category) async {
+    Database db = await instance.database;
+    return await db.query(table, where: '$columnCategory =?',whereArgs: [category]);
+  }
 }
